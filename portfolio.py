cc64c54dfb49e7151006b7557dfa92efa82bc1a2
sunny-gao-123 <gaoyajing1997@163.com>
2021-11-16 10:16:22 -0500

    buy and sell function + testing
    practice how to do software testing and how to use github

--- portfolio.py
+++ portfolio.py
@@ -1,10 +1,38 @@
+class Shares:
+    def __init__(self,name,shares,price):
+        self.name=name
+        self.shares =shares
+        self.price=price
+
+
 class Portfolio:
     def __init__(self):
         self._stocks=[]
     # need to save what have bought somewhere 
 
-    def buy(self,name,shares,price):
-        self._stocks.append((name,shares,price))
+    def buy(self,Shares):
+        self._stocks.append(Shares)
+
+    def buy_many(self,List_of_stocks):
+        for stock in List_of_stocks:
+            self.buy(stock)
     
     def cost(self):
-        return sum(shares * price for _,shares,price in self._stocks)
+        return sum(Shares.shares * Shares.price for Shares in self._stocks)
+
+    def sell(self,stock_name,shares_to_sold):
+        # if shares_to_sold is negative --> buy 
+        for Stock in self._stocks:
+            if Stock.name == stock_name:
+                if Stock.shares < shares_to_sold:
+                    raise ValueError('do not have enough stocks to sell')
+                
+                Stock.shares = Stock.shares - shares_to_sold
+                break
+        else:        
+            raise ValueError('you do not own that stock')
+ #else: it is executed when the loop terminates through exhaustion of the iterable (with for) 
+ #                     but not when the loop is terminated by a break statement
+
+
+    
--- test_portfolio.py
+++ test_portfolio.py
@@ -1,9 +1,47 @@
-# test_portfolio.py
-from portfolio import Portfolio
+#test portfolio using pytest
+from portfolio import Portfolio,Shares
+import pytest
 
-p = Portfolio()
-print(f"Empty portfolio cost: {p.cost()}")
-p.buy("IBM", 100, 176.48)
-print(f"With 100 IBM @ 176.48: {p.cost()}")
-p.buy("HPQ", 100, 36.15)
-print(f"With 100 HPQ @ 36.15: {p.cost()}")
+def test_buy_one_stock():
+    p = Portfolio()
+    a= Shares("IBM", 100, 176.48)
+    p.buy(a)
+    assert p.cost() == 17648.0
+
+def test_empty_portfolio():
+    p = Portfolio()
+    assert p.cost()==0.0
+
+def test_buy_multiple_stocks():
+    p = Portfolio()
+    a= Shares("IBM", 100, 176.48)
+    b= Shares("HPQ", 100, 36.15)
+    p.buy(a)
+    p.buy(b)
+    assert p.cost()==21263.0
+
+
+#A fixture is a function to create the required initial state. Pytest will find and execute them as needed.
+#The fixture is a function decorated with @pytest.fixture. 
+
+@pytest.fixture
+def simple_portfolio():
+    p = Portfolio()
+    a=Shares("MSFT", 100, 27.0)        
+    b=Shares("DELL", 100, 17.0)        
+    c=Shares("ORCL", 100, 34.0)  
+    p.buy_many([a,b,c])
+    return p
+
+
+def test_sell_stock(simple_portfolio):
+    simple_portfolio.sell("MSFT", 50)
+    assert simple_portfolio.cost()== 6450.0
+
+def test_not_enough(simple_portfolio):
+    with pytest.raises(ValueError):
+        simple_portfolio.sell("MSFT", 200)
+
+def test_dont_own_it(simple_portfolio):
+    with pytest.raises(ValueError):
+        simple_portfolio.sell("pmg", 1)
